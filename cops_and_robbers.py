3d3f1c29da341fd4fe8ae07ae097ce7170ebfa9d
mfurmane <mfurmane9@gmail.com>
2016-04-06 23:10:43 +0200

    walk strategies prototype in progress

--- cops_and_robbers.py
+++ cops_and_robbers.py
@@ -6,7 +6,8 @@ import pylab
 graph_representation = {0: [1, 4], 1: [0, 2, 5], 2: [1, 3, 6], 3: [2, 7],
                         4: [0, 5, 8], 5: [1, 4, 6, 9], 6: [2, 5, 7, 10], 7: [3, 6, 11],
                         8: [4, 9, 12], 9: [5, 8, 10, 13], 10: [6, 9, 11, 14], 11: [7, 10, 15],
-                        12: [8, 13], 13: [9, 12, 14], 14: [10, 13, 15], 15: [11, 14]}
+                        12: [8, 13], 13: [9, 12, 14], 14: [10, 13, 15], 15: [11, 14],
+                        16: [3]}
 
 
 class Graph:
@@ -117,38 +118,101 @@ class Robber(Guy):
         Guy.__init__(self, position, name, graph)
         self.graph.add_robber(self)
 
+class RStrategy:
+    def next_move(self, who): raise NotImplementedError
+    
+class CStrategy:
+    def next_move(self, who, where): raise NotImplementedError
+
+class RRTstrategy(RStrategy):
+    def __init__(self, target):
+        self.target = target
+        print "    - choosen strategy: RRT"
+    def next_move(self, who):
+        all_paths = nx.all_pairs_shortest_path(who.graph.networkx_graph())
+        all_paths[who.position]
+        print all_paths[who.position][self.target]
+        if len(all_paths[who.position][self.target]) > 1: return all_paths[who.position][self.target][1]
+        
+class SimpleStrategy(CStrategy):
+    def __init__(self):
+        print "    - choosen strategy: simple"
+    def next_move(self, who):
+        
+        print ""
+    
 
 if __name__ == "__main__":
     pylab.ion()
     graph = Graph(graph_representation)
+    
+    print "ROBBER:"
+    robber_strategy = RRTstrategy(3)
+    print "COP:"
+    cop_strategy = RRTstrategy(9)
 
     cop1 = Cop(3, "Janusz", graph)
     robber1 = Robber(5, "Miroslaw", graph)
     pylab.show()
-    game_on = True
+    game_on = True    
+    graph.plot_graph()
+    pylab.draw()
+    
     while game_on:
         rp_cop = graph.random_walk_on_graph(cop1.position)
         rp_robber = graph.random_walk_on_graph(robber1.position)
+        #~ print rp_cop, rp_robber
+        
+        
+        
+        plt.pause(1)  
+        if robber1.position == cop1.position:
+            game_on = False
+            print "GAME OVER!"
+            break
+        elif robber1.position == robber_strategy.target:
+            game_on = False
+            print "VICTORY!"
+            break
+        robber1.change_position(robber_strategy.next_move(robber1))
+        graph.plot_graph()
+        pylab.draw()
+        plt.pause(1)
+        if robber1.position == cop1.position:
+            game_on = False
+            print "GAME OVER!"
+            break
+        elif robber1.position == robber_strategy.target:
+            game_on = False
+            print "VICTORY!"
+            break
+        cop1.change_position(cop_strategy.next_move(cop1))
+        graph.plot_graph()
+        pylab.draw()
+            
+            
+            
+            
+            
+        #~ for i in xrange(min(len(rp_cop), len(rp_robber))):
+            #~ if robber1.position == cop1.position:
+                #~ game_on = False
+                #~ break
+            #~ if i < len(rp_robber):
+                #~ if rp_robber[i] != cop1.position:
+                    #~ robber1.change_position(rp_robber[i])
+            #~ graph.plot_graph()
+            #~ pylab.draw()
+            #~ plt.pause(0)
+            #~ if robber1.position == cop1.position:
+                #~ game_on = False
+                #~ break
+            #~ if i < len(rp_cop):
+                #~ cop1.change_position(rp_cop[i])
+            #~ graph.plot_graph()
+            #~ pylab.draw()
+            #~ plt.pause(0)
 
-        for i in xrange(min(len(rp_cop), len(rp_robber))):
-            if robber1.position == cop1.position:
-                game_on = False
-                break
-            if i < len(rp_robber):
-                if rp_robber[i] != cop1.position:
-                    robber1.change_position(rp_robber[i])
-            graph.plot_graph()
-            pylab.draw()
-            plt.pause(1)
-            if robber1.position == cop1.position:
-                game_on = False
-                break
-            if i < len(rp_cop):
-                cop1.change_position(rp_cop[i])
-            graph.plot_graph()
-            pylab.draw()
-            plt.pause(1)
-
-    print "GAME OVER!"
+    
 
 
